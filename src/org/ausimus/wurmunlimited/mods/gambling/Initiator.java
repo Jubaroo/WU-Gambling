12c65daca5d3bee105c1cc22972b2c1f071abd32
ausimus <ausimus1@gmail.com>
2017-08-04 18:57:19 -0400

    Added serverPacks property for custom model. Updated Z position of Gambling Machine model. Added Log Writer to main Initiator.

--- src/org/ausimus/wurmunlimited/mods/gambling/Initiator.java
+++ src/org/ausimus/wurmunlimited/mods/gambling/Initiator.java
@@ -18,6 +18,12 @@ import org.ausimus.wurmunlimited.mods.gambling.items.GItemTemplateCreator;
 import org.gotti.wurmunlimited.modloader.interfaces.*;
 import org.gotti.wurmunlimited.modsupport.actions.ModActions;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Properties;
 
 public class Initiator implements WurmServerMod, Configurable, PreInitable, ServerStartedListener, ServerPollListener, ItemTemplatesCreatedListener {
@@ -36,6 +42,7 @@ public class Initiator implements WurmServerMod, Configurable, PreInitable, Serv
     @Override
     public void preInit() {
         ModActions.init();
+        WriteLog("ModActions Initiated.");
     }
 
 
@@ -46,10 +53,29 @@ public class Initiator implements WurmServerMod, Configurable, PreInitable, Serv
     @Override
     public void onServerStarted() {
         new ActionsInit();
+        WriteLog("Actions Registered.");
     }
 
     @Override
     public void onItemTemplatesCreated() {
         new GItemTemplateCreator();
+        WriteLog("Templates Created.");
+    }
+
+    private void WriteLog(String data) {
+        try {
+            String logFile = "mods/GambleMod/log.txt";
+            FileWriter writeLog = new FileWriter(logFile, true);
+            BufferedWriter bufferedLogWriter = new BufferedWriter(writeLog);
+            DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+            Date timeStamp = new Date();
+            bufferedLogWriter.write("==========================================================\n");
+            bufferedLogWriter.write(dateFormat.format(timeStamp) + "\n");
+            bufferedLogWriter.write(data + "\n");
+            bufferedLogWriter.write("==========================================================\n");
+            bufferedLogWriter.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 }
